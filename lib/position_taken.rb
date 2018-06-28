1c131c64106f828ddade63d9c0bc2516c5a67f9c
T Law <redirect@gmail.com>
2018-06-28 17:48:34 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,12 +1,11 @@
 # code your #position_taken? method here!
 
  
-def position_taken(board, index)
+def position_taken?(board, index)
   if board[index] == " " || if board[index] == "" || if board[index] == nil
       return false
   elsif board[index] == "X" || if board[index] == "O"
      return true
-  end
 end
 
   
